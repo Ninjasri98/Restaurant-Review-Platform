cbfe78bba6766a148dbb3d545ab60f738de1fc02
Ninjasri98 <palitsriparno@gmail.com>
2026-01-01 11:18:08 +0530

    added storage service with custom exceptions

---
+++ restaurant/src/main/java/com/ninjasri98/restaurant/services/StorageService.java
@@ -0,0 +1,13 @@
+package com.ninjasri98.restaurant.services;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.Optional;
+
+public interface StorageService {
+    // Store a file and return its unique identifier
+    String store(MultipartFile file, String filename);
+
+    // Retrieve a file by its identifier
+    Optional<Resource> loadAsResource(String filename);
+}
