1d73e8010f71596074d576486c50d9bf21178c57
Ninjasri98 <palitsriparno@gmail.com>
2026-01-02 10:46:17 +0530

    implemented photo retrieval endpoint

--- restaurant/src/main/java/com/ninjasri98/restaurant/config/SecurityConfig.java
+++ restaurant/src/main/java/com/ninjasri98/restaurant/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ninjasri98.restaurant.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
